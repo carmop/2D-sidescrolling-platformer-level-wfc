40f5c18e5a80b929e2ba49ed423de118606ed64d
carmop <carmop@allegheny.edu>
2024-02-21 18:52:35 -0500

    Add config data

---
+++ WFCinPython/Settings.py
@@ -0,0 +1,21 @@
+#Controls
+
+INTERACTIVE = True
+INTERACTIVE_KEYPRESS = True
+WEIGHTED = True
+
+#Path to spritesheet
+SPRITESHEET_PATH = "images/debug_sprites.png"
+
+#Amount of tiles per level
+LEVEL_LENGTH = 15
+LEVEL_H = 1
+
+#Tile sprites are 4 pixels tall and 3 pixels wide
+Y_TILE = 16
+X_TILE = 16
+
+#Upscaling factor
+SCALING = 10
+X_SCALE = 10
+Y_SCALE = 10
