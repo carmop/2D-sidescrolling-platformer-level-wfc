984c4056a92c35501e16b13427ca133a7c8ac6b1
carmop <carmop@allegheny.edu>
2024-02-21 19:02:43 -0500

    Add init

--- WFCinPython/Level.py
+++ WFCinPython/Level.py
@@ -7,3 +7,27 @@ class Level:
     """
     
     """
+
+    def __init__(self, sizeX, sizeY) -> None:
+        
+        self.cols = sizeX
+        self.rows = sizeY
+
+        self.tileRows = []
+        for y in range(sizeY):
+            tiles = []
+            for x in range(sizeX):
+                tile = Tile(x,y)
+                tiles.append(tile)
+            self.tileRows.append(tiles)
+
+        for y in range(sizeY):
+            for x in range(sizeX):
+                tile = self.tileRows[y][x]
+
+                # Add neighbouring tiles to a list, 
+                # for checking left and right sides of current tile for propagating probability
+                if x < sizeX - 1:
+                    tile.addNeighbour(RIGHT, self.tileRows[y][x+1])
+                if x > 0: 
+                    tile.addNeighbour(LEFT, self.tileRows[y][x-1])
