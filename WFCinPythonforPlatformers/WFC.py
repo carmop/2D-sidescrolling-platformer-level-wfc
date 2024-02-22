e521645f2636b63c6766d8a6af3740f006616a3c
carmop <carmop@allegheny.edu>
2024-02-21 22:02:24 -0500

    Change comments

--- WFCinPythonforPlatformers/WFC.py
+++ WFCinPythonforPlatformers/WFC.py
@@ -4,30 +4,29 @@ from Draw import DrawLevel
 from Settings import *
 
 
-#Set whether or not the level generator will be interactive from settings file
+# Set whether or not the level generator will be interactive from settings file.
 interact = INTERACTIVE
 key_press = INTERACTIVE_KEYPRESS
 
 pygame.init()
 clock = pygame.time.Clock()
 
-#Create surface object, base on size of level and based on level length and y size of 1 tile
+# Create surface object, base on size of level and based on level length and y size of 1 tile.
 displaySurface = pygame.display.set_mode(size=((LEVEL_LENGTH * X_TILE * SCALING) + 5, (LEVEL_H * Y_TILE * SCALING)))
-# displaySurface = pygame.display.set_mode(((WORLD_X * TILESIZE * SCALETILE) + 10, (WORLD_Y * TILESIZE * SCALETILE) + 10), flags=pygame.FULLSCREEN)
 
 pygame.display.set_caption("WFC Algo")
 
-#Create level variable to determine level size from settings file
+# Create level variable to determine level size from settings file.
 level = Level(LEVEL_LENGTH, LEVEL_H)
-# level = Level(10, 1) #debug/test value
 
-#Create level based on size of 'level' variable
+
+# Create level based on size of 'level' variable.
 drawLevel = DrawLevel(level)
 
-#Checks is level has benn completely finished
+# Checks is level has benn completely finished.
 done = False
 
-#Instantly draws level
+# Instantly draws level if `interact` is set to False.
 if interact is False:
     while done is False:
         result = level.waveFunctionCollapse()
@@ -37,7 +36,7 @@ if interact is False:
 drawLevel.update()
 isRunning = True
 
-#Draws level in real time
+# Draws level in real time or by pressing space key if `interact` is set to True.
 while isRunning:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
