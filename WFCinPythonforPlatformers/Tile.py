9530ceeb31dd3328bce07df839a241f55afb0a4c
carmop <carmop@allegheny.edu>
2024-02-21 20:23:20 -0500

    Add functions for tile referencing

--- WFCinPythonforPlatformers/Tile.py
+++ WFCinPythonforPlatformers/Tile.py
@@ -2,8 +2,38 @@ import random
 from Settings import *
 
 class Tile:
-    """"""
+    """
+    
+    """
+
+
     def __init__(self, x, y):
+        """"""
+        
         self.possibilities = list(tileRules.keys())
         self.entropy = len(self.possibilities)
         self.neighbours = dict()
+
+
+    def addNeighbour(self, direction, tile):
+        """"""
+
+        self.neighbours[direction] = tile
+
+
+    def getNeighbour(self, direction):
+        """"""
+
+        return self.neighbours[direction]
+
+
+    def getDirections(self):
+        """"""
+
+        return list(self.neighbours.keys())
+
+
+    def getPossibilities(self):
+        """"""
+
+        return self.possibilities
