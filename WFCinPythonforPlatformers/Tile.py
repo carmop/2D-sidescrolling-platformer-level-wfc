382fbe6b622fe06790480b640a1ed2d4cb5701e4
carmop <carmop@allegheny.edu>
2024-02-21 20:21:18 -0500

    Add init

--- WFCinPythonforPlatformers/Tile.py
+++ WFCinPythonforPlatformers/Tile.py
@@ -1,2 +1,9 @@
 import random
 from Settings import *
+
+class Tile:
+    """"""
+    def __init__(self, x, y):
+        self.possibilities = list(tileRules.keys())
+        self.entropy = len(self.possibilities)
+        self.neighbours = dict()
