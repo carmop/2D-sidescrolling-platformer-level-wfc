554b65873b45afec574893301cb8af2aa25d1972
carmop <carmop@allegheny.edu>
2024-02-21 20:26:58 -0500

    Add collapse function

--- WFCinPythonforPlatformers/Tile.py
+++ WFCinPythonforPlatformers/Tile.py
@@ -9,7 +9,7 @@ class Tile:
 
     def __init__(self, x, y):
         """"""
-        
+
         self.possibilities = list(tileRules.keys())
         self.entropy = len(self.possibilities)
         self.neighbours = dict()
@@ -37,3 +37,16 @@ class Tile:
         """"""
 
         return self.possibilities
+    
+
+    def collapse(self):
+        """"""
+
+        if WEIGHTED:
+            weight = [tileWeights[possibility] for possibility in self.possibilities]
+            self.possibilities = random.choices(self.possibilities, weights=weight, k=1)
+            self.entropy = 0
+        else:
+            self.possibilities = random.choices(self.possibilities, weights=None, k=1)
+            self.entropy = 0
+
