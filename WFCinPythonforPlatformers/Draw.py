4f4cf3ea313610a3e3164d99c5696fdc69bd598d
carmop <carmop@allegheny.edu>
2024-02-21 20:07:44 -0500

    Add function to update visualization

--- WFCinPythonforPlatformers/Draw.py
+++ WFCinPythonforPlatformers/Draw.py
@@ -18,3 +18,48 @@ class DrawLevel:
 
         self.level = level
         self.surface = pygame.Surface(((LEVEL_LENGTH * X_TILE * SCALING), (LEVEL_H * Y_TILE * SCALING)))
+
+
+    def update(self):
+        """"""
+        lowest_entropy = self.level.getLowestEntropy()
+
+        for y in range(LEVEL_H):
+             for x in range(LEVEL_LENGTH):
+                  tile_entropy = self.level.getEntropy(x, y)
+                  tile_type = self.level.getType(x, y)
+                
+                  if tile_entropy > 0:
+                        tile_image = pygame.Surface((X_TILE, Y_TILE))
+                        if tile_entropy == 27:
+                            textSurface = self.font2.render(str(tile_entropy), True, "darkgrey")
+                            tile_image.blit(textSurface, (3, 3))
+                        elif tile_entropy >= 10:
+                            textSurface = self.font1.render(str(tile_entropy), True, "grey")
+                            tile_image.blit(textSurface, (2, 3))
+                        elif tile_entropy < 10:
+                            if tile_entropy == lowest_entropy:
+                                textSurface = self.font0.render(str(tile_entropy), True, "green")
+                            else:
+                                textSurface = self.font0.render(str(tile_entropy), True, "white")
+                            tile_image.blit(textSurface, (4, 1))
+                    #
+                            
+                  else:  
+                    pos = tileSprites[EASY_STRAIGHT_MID] # Use 1 instead of EASY_STRAIGHT_MID ?
+                    tile_image = self.spritesheet.subsurface(pygame.Rect(pos[0], pos[1], X_TILE, Y_TILE))
+                    tile_image = pygame.transform.scale_by(tile_image, (SCALING, SCALING))
+                    self.surface.blit(tile_image, (x * X_TILE * SCALING, y * Y_TILE * SCALING))
+                    pos = tileSprites[tile_type]
+                    tile_image = self.spritesheet.subsurface(pygame.Rect(pos[0], pos[1], X_TILE, Y_TILE))
+                
+                  tile_image = pygame.transform.scale_by(tile_image, (SCALING, SCALING))
+                  self.surface.blit(tile_image, (x * X_TILE * SCALING, y * Y_TILE * SCALING))
+
+
+    def draw(self, displaySurface):
+     
+     displaySurface.blit(self.surface, (0, 0))
+
+                            
+                        
