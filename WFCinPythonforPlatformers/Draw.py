94e1301336b15bc54e5c46a04cdd8f54f5a37879
carmop <carmop@allegheny.edu>
2024-02-21 20:20:39 -0500

    Add documentation

--- WFCinPythonforPlatformers/Draw.py
+++ WFCinPythonforPlatformers/Draw.py
@@ -3,11 +3,14 @@ from Settings import *
 
 
 class DrawLevel:
-    """
-    
+    """Pygame code for drawing the WFC generated level.
+
+    Uses pygame builty in functionality to create game window,
+    draw level grid, write entropy values on each grid cell,
+    and finally draw the collapsed tiles.  
     """
     def __init__(self, level) -> None:
-        """"""
+        """Chose fonts and load image path."""
 
         #Text fonts from pygame package 
         self.font0 = pygame.font.Font(pygame.font.get_default_font(), 14)
@@ -21,43 +24,50 @@ class DrawLevel:
 
 
     def update(self):
-        """"""
+        """Get tiles' entropy to display its numeric value in the cells, and draw collapsed tiles."""
+
         lowest_entropy = self.level.getLowestEntropy()
 
         for y in range(LEVEL_H):
              for x in range(LEVEL_LENGTH):
-                  tile_entropy = self.level.getEntropy(x, y)
-                  tile_type = self.level.getType(x, y)
+                tile_entropy = self.level.getEntropy(x, y)
+                tile_type = self.level.getType(x, y)
                 
-                  if tile_entropy > 0:
-                        tile_image = pygame.Surface((X_TILE, Y_TILE))
-                        if tile_entropy == 27:
-                            textSurface = self.font2.render(str(tile_entropy), True, "darkgrey")
-                            tile_image.blit(textSurface, (3, 3))
-                        elif tile_entropy >= 10:
-                            textSurface = self.font1.render(str(tile_entropy), True, "grey")
-                            tile_image.blit(textSurface, (2, 3))
-                        elif tile_entropy < 10:
-                            if tile_entropy == lowest_entropy:
-                                textSurface = self.font0.render(str(tile_entropy), True, "green")
-                            else:
-                                textSurface = self.font0.render(str(tile_entropy), True, "white")
-                            tile_image.blit(textSurface, (4, 1))
-                    #
-                            
-                  else:  
+                # Different entropy values will be written with different colors.
+                if tile_entropy > 0:
+                    tile_image = pygame.Surface((X_TILE, Y_TILE))
+                    if tile_entropy == 27:
+                        textSurface = self.font2.render(str(tile_entropy), True, "darkgrey")
+                        tile_image.blit(textSurface, (3, 3))
+                    elif tile_entropy >= 10:
+                        textSurface = self.font1.render(str(tile_entropy), True, "grey")
+                        tile_image.blit(textSurface, (2, 3))
+                    elif tile_entropy < 10:
+                        if tile_entropy == lowest_entropy:
+                            textSurface = self.font0.render(str(tile_entropy), True, "green")
+                        else:
+                            textSurface = self.font0.render(str(tile_entropy), True, "white")
+                        tile_image.blit(textSurface, (4, 1))
+
+                # When entropy is 0 tile will be collapsed so it has to be drawn.            
+                else:
                     pos = tileSprites[EASY_STRAIGHT_MID] # Use 1 instead of EASY_STRAIGHT_MID ?
+
+                    # Get image from spritesheet and scale properly.
                     tile_image = self.spritesheet.subsurface(pygame.Rect(pos[0], pos[1], X_TILE, Y_TILE))
                     tile_image = pygame.transform.scale_by(tile_image, (SCALING, SCALING))
+
+                    # Draw tile in correct positon.
                     self.surface.blit(tile_image, (x * X_TILE * SCALING, y * Y_TILE * SCALING))
                     pos = tileSprites[tile_type]
                     tile_image = self.spritesheet.subsurface(pygame.Rect(pos[0], pos[1], X_TILE, Y_TILE))
                 
-                  tile_image = pygame.transform.scale_by(tile_image, (SCALING, SCALING))
-                  self.surface.blit(tile_image, (x * X_TILE * SCALING, y * Y_TILE * SCALING))
+                tile_image = pygame.transform.scale_by(tile_image, (SCALING, SCALING))
+                self.surface.blit(tile_image, (x * X_TILE * SCALING, y * Y_TILE * SCALING))
 
 
     def draw(self, displaySurface):
+     """Draws the level."""
      
      displaySurface.blit(self.surface, (0, 0))
 
