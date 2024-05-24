47b810b11034ce09c4386f67b0667e4318eb5c2f
carmop <carmop@allegheny.edu>
2024-05-24 12:00:11 -0400

    Add main function code

---
+++ WFC/main.py
@@ -0,0 +1,19 @@
+from WFC import run
+from Control import parse
+from Settings import *
+
+def main():
+    """"""
+    parameters=parse()
+
+    interact = parameters['instant']
+    key_press = parameters['press']
+    weighted = parameters['weight']
+    use_curve = parameters['ac']
+
+    # some code for settings possibly?
+
+    print(parameters)
+    print('aight')
+
+main()
