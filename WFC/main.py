50da3948f1bfd0d43a774449d0e7e46dff47ec37
carmop <carmop@allegheny.edu>
2024-05-30 10:05:10 -0400

    Add to main()

--- WFC/main.py
+++ WFC/main.py
@@ -3,10 +3,11 @@ from Control import parse
 
 
 def main():
-    """"""
+    """Function for handling argparse inputs for level generation and calling level generator."""
+
     parameters=parse()
 
-    interact = parameters['instant']
+    interact = parameters['progress']
     key_press = parameters['press']
     weighted = parameters['weight']
     use_curve = parameters['ac']
@@ -14,10 +15,10 @@ def main():
     levels = parameters['levels']
     save = parameters['save']
 
-    # some code for settings possibly?
-
-    print(parameters)
-    print('aight')
+    print('Generation Using Following Parameters:')
+    # print(parameters)
+    for item in parameters:
+        print(item,':',parameters[item])
 
     run(levels, weighted, use_curve, interact, key_press, save)
 
